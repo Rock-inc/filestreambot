52919e557c7b3be53c18f87687eb23bfd1a66663
Rock-inc <54385517+Rock-inc@users.noreply.github.com>
2022-07-13 11:45:50 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -7,19 +7,19 @@ load_dotenv()
 
 
 class Var(object):
-    API_ID = int(getenv('API_ID'))
-    API_HASH = str(getenv('API_HASH'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN'))
-    SESSION_NAME = str(getenv('SESSION_NAME', 'F2LxBot'))
+    API_ID = int('2278054')
+    API_HASH = str('61204e9922b08b3cfaab70b099703448')
+    BOT_TOKEN = str('5516450836:AAEFMrS8oWOWJ0hZpGbdNxswWSbCvm4O2qM')
+    SESSION_NAME = str('Codexmania')
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL'))
+    BIN_CHANNEL = int('-1001650921796')
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
-    OWNER_ID = int(getenv('OWNER_ID', '2068472689'))
+    OWNER_ID = int(getenv('OWNER_ID', '1246418936')
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME'))
+    OWNER_USERNAME = str('@missrose_bot')
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME'))
@@ -28,6 +28,7 @@ class Var(object):
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
     URL = "https://{}/".format(FQDN) if ON_HEROKU or NO_PORT else \
         "http://{}:{}/".format(FQDN, PORT)
-    DATABASE_URL = str(getenv('DATABASE_URL'))
+    DATABASE_URL = str('mongodb+srv://0000:0000@atlascluster.euseb.mongodb.net/?retryWrites=true&w=majority')
+
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', None))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split()))
